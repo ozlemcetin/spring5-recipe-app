a0beece0319aaa72433ecbc8bda88d502c1a11ab
ozlem <ozlem.cetin@gmail.com>
2020-04-09 17:14:10 +0300

    One to One JPA Relationships. Creating domain objects of Notes and Recipe.

---
+++ src/main/java/guru/springframework/spring5recipeapp/domain/Notes.java
@@ -0,0 +1,44 @@
+package guru.springframework.spring5recipeapp.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Notes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private Recipe recipe;
+
+    /*
+    JPA gets a CLOB filed in the database to persits the notes w/o worrying about limitations on the size of the String.
+     */
+    @Lob
+    private String recipeNotes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public String getRecipeNotes() {
+        return recipeNotes;
+    }
+
+    public void setRecipeNotes(String recipeNotes) {
+        this.recipeNotes = recipeNotes;
+    }
+}
