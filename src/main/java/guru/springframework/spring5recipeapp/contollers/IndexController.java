3afa4b312edc3c5abc3c1d19046063c06516b7b0
ozlem <ozlem.cetin@gmail.com>
2020-04-09 21:25:10 +0300

    Spring Data JPA Query Methods

--- src/main/java/guru/springframework/spring5recipeapp/contollers/IndexController.java
+++ src/main/java/guru/springframework/spring5recipeapp/contollers/IndexController.java
@@ -1,7 +1,13 @@
 package guru.springframework.spring5recipeapp.contollers;
 
+import guru.springframework.spring5recipeapp.domain.Category;
+import guru.springframework.spring5recipeapp.domain.UnitOfMeasure;
+import guru.springframework.spring5recipeapp.repositories.CategoryRepository;
+import guru.springframework.spring5recipeapp.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 /**
  * Created by jt on 6/1/17.
@@ -9,8 +15,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
+    private final CategoryRepository categoryRepository;
+    private final UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"", "/", "/index"})
-    public String getIndexPage(){
+    public String getIndexPage() {
+
+        Optional<Category> optionalCategory =
+                categoryRepository.findByDescription("American");
+        optionalCategory.ifPresent(e -> System.out.println("Category Id: " + e.getId()));
+
+        Optional<UnitOfMeasure> optionalUnitOfMeasure =
+                unitOfMeasureRepository.findByDescription("Pinch");
+        optionalUnitOfMeasure.ifPresent(e -> System.out.println("Measure Id: " + e.getId()));
+
         return "index";
     }
 }
--- src/main/java/guru/springframework/spring5recipeapp/repositories/CategoryRepository.java
+++ src/main/java/guru/springframework/spring5recipeapp/repositories/CategoryRepository.java
@@ -3,5 +3,14 @@ package guru.springframework.spring5recipeapp.repositories;
 import guru.springframework.spring5recipeapp.domain.Category;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+
+    /*
+    Create a method name using a property using find, find by property name.
+    Spring Data JPA is going to go in, create that query based on the criteria found inside the method name.
+    This is  feature of Spring Data JPA, it saves us a lot of work and time.
+     */
+    Optional<Category> findByDescription(String description);
 }
