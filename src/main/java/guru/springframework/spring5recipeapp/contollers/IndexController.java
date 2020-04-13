8660f3c86192a52a52ed36a05e3e0badc94efb18
ozlem <ozlem.cetin@gmail.com>
2020-04-13 14:12:26 +0300

    Using Bootstrap CSS

--- src/main/java/guru/springframework/spring5recipeapp/contollers/IndexController.java
+++ src/main/java/guru/springframework/spring5recipeapp/contollers/IndexController.java
@@ -1,7 +1,9 @@
 package guru.springframework.spring5recipeapp.contollers;
 
+import guru.springframework.spring5recipeapp.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -11,11 +13,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
+    private final RecipeService recipeService;
+
+    public IndexController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+
 
     @RequestMapping({"", "/", "/index"})
-    public String getIndexPage() {
+    public String getIndexPage(Model model) {
 
-        log.debug("IndexController > Returns Index Page.");
+        log.debug("IndexController > Returns Recipes List.");
+        model.addAttribute("recipes", recipeService.getRecipes());
         return "index";
     }
 }
