d5b23c316aaf242cee07c50c6d1c879320cc250d
ozlemcetin <ozlem.cetin@gmail.com>
2022-09-02 20:52:43 +0300

    Spring Data JPA Query Methods Closes #8

--- src/main/java/guru/springframework/spring5recipeapp/controllers/IndexController.java
+++ src/main/java/guru/springframework/spring5recipeapp/controllers/IndexController.java
@@ -1,14 +1,28 @@
 package guru.springframework.spring5recipeapp.controllers;
 
+import guru.springframework.spring5recipeapp.repositories.CategoryRepository;
+import guru.springframework.spring5recipeapp.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 
 @Controller
 public class IndexController {
 
+    private final CategoryRepository categoryRepository;
+    private final UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
+
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage() {
+
+        System.out.println("Category Id : " + categoryRepository.findByDescription("American").get().getId());
+        System.out.println("Measure Id: " + unitOfMeasureRepository.findByDescription("Teaspoon").get().getId());
+
         return "index";
     }
 }
